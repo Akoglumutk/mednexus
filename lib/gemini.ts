ae4451869b62fe2e51df59dc38a54115061570f7
Akogluumutk <112826767+Akoglumutk@users.noreply.github.com>
2026-05-01 00:06:20 +0300

    Update gemini.ts

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "");
 
 export const medicalModel = genAI.getGenerativeModel({ 
-  model: "gemini-1.5-flash", // Daha stabil referans
+  model: "gemini-flash-latest", // Daha stabil referans
   generationConfig: { 
     responseMimeType: "application/json",
     temperature: 0.7
